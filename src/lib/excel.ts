d5376c891cf4ab5262b59452abaf745ddf87d706
plastic041 <plastic041@gmail.com>
2023-11-21 15:25:26 +0900

    feat: 주석

--- src/lib/excel.ts
+++ src/lib/excel.ts
@@ -4,6 +4,9 @@ import type { FetchResponse } from "./mock-response";
 import type { Item } from "./mock-items";
 import type { Reservation } from "./mock-reservations";
 
+/**
+ * 요약 정보를 생성합니다.
+ */
 function makeInfoRows(info: { factory: string; date: string }) {
   return [
     ["지역", info.factory],
@@ -11,6 +14,9 @@ function makeInfoRows(info: { factory: string; date: string }) {
   ];
 }
 
+/**
+ * 상품 정보 요약 줄을 생성합니다.
+ */
 function makeItemRows(items: Item[]) {
   // 메뉴, 총계, ...items.names
   // 수량, 총계 수량, ...items.counts
@@ -26,15 +32,19 @@ function makeItemRows(items: Item[]) {
 }
 
 type ReservationRows = (string | number)[][];
-
+/**
+ * 신청 정보 요약 줄을 생성합니다.
+ * 배송지별, 메뉴별로 묶어서 생성합니다.
+ */
 function makeReservationsRows(reservations: Reservation[]): ReservationRows {
   // 배송지ID, 배송지, 메뉴, 신청인, 직번, 수량
+
+  // 배송지ID별로 묶기
   const deliveryAddressIds = [
     ...new Set(
       reservations.map((reservation) => reservation.deliveryAddressId)
     ),
   ];
-
   const rows: ReservationRows = [];
   for (const deliveryAddressId of deliveryAddressIds) {
     const foundReservations = reservations.filter(
@@ -46,41 +56,27 @@ function makeReservationsRows(reservations: Reservation[]): ReservationRows {
     const summaryRow = ["배송지ID", "배송지", "메뉴", "신청인", "직번", "수량"];
     rows.push(summaryRow);
 
+    // 배송지ID별로 묶인 것 중에서 메뉴별로 묶기
     const itemNames = [
       ...new Set(foundReservations.map((reservation) => reservation.itemName)),
     ];
-    console.log(itemNames);
-
     for (const itemName of itemNames) {
-      const foundReservationsItemNames = reservations.filter(
+      const foundReservationsItemNames = foundReservations.filter(
         (reservation) => reservation.itemName === itemName
       );
-      const firstReservation = foundReservationsItemNames[0];
-      const summaryRow = [
-        firstReservation.deliveryAddressId,
-        firstReservation.deliveryAddress,
-        firstReservation.itemName,
-        "---",
-        "---",
-        sum(foundReservationsItemNames.map((reservation) => reservation.count)),
-      ];
-      rows.push(summaryRow);
 
-      rows.push(
-        ...foundReservationsItemNames.map((reservation) => [
-          reservation.deliveryAddressId,
-          reservation.deliveryAddress,
-          reservation.itemName,
-          reservation.reserver.name,
-          reservation.reserver.id,
-          reservation.count,
-        ])
+      const perItemRows = makeReservationsPerItemsRows(
+        foundReservationsItemNames
       );
+
+      rows.push(...perItemRows);
     }
 
-    const totalCountRow = ["", "", "", "", "총계", totalCount];
+    const totalCountRow = ["", "", "", "", "총계", totalCount]; // 메뉴별 신청 목록 총계
 
     rows.push(totalCountRow);
+
+    // 여백 3줄
     rows.push([]);
     rows.push([]);
     rows.push([]);
@@ -89,6 +85,49 @@ function makeReservationsRows(reservations: Reservation[]): ReservationRows {
   return rows;
 }
 
+/**
+ * 신청 정보 요약 줄을 생성합니다.
+ */
+function makeReservationsPerItemsRows(reservations: Reservation[]) {
+  const rows = [];
+
+  // 배송지ID별로 묶인 것 중에서 메뉴별로 묶기
+  const itemNames = [
+    ...new Set(reservations.map((reservation) => reservation.itemName)),
+  ];
+  for (const itemName of itemNames) {
+    const foundReservationsItemNames = reservations.filter(
+      (reservation) => reservation.itemName === itemName
+    );
+    const firstReservation = foundReservationsItemNames[0];
+    const summaryRow = [
+      firstReservation.deliveryAddressId,
+      firstReservation.deliveryAddress,
+      firstReservation.itemName,
+      "---",
+      "---",
+      sum(foundReservationsItemNames.map((reservation) => reservation.count)),
+    ];
+    rows.push(summaryRow); // 메뉴별 요약
+
+    rows.push(
+      ...foundReservationsItemNames.map((reservation) => [
+        reservation.deliveryAddressId,
+        reservation.deliveryAddress,
+        reservation.itemName,
+        reservation.reserver.name,
+        reservation.reserver.id,
+        reservation.count,
+      ])
+    ); // 메뉴별 신청 목록
+  }
+
+  return rows;
+}
+
+/**
+ * 엑셀 파일을 생성합니다.
+ */
 export function createExcelFile(data: FetchResponse) {
   const infoRows = makeInfoRows(data);
   const itemRows = makeItemRows(data.items);
@@ -101,6 +140,9 @@ export function createExcelFile(data: FetchResponse) {
   return workbook;
 }
 
+/**
+ * 엑셀 파일을 다운로드합니다.
+ */
 export function downloadXlsx(workbook: XLSX.WorkBook, filename: string) {
   XLSX.writeFile(workbook, filename);
 }
